04fd61918d9388bc455138ad5ff2f1c9be7fa81a
OzanOcak <ozanocak@gmail.com>
2024-06-04 14:16:21 -0400

    selector

--- src/stores/catStore.tsx
+++ src/stores/catStore.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createSelectors } from "../utils/createSelectors";
 
 type TCatStoreState = {
   cats: {
@@ -10,21 +11,23 @@ type TCatStoreState = {
   summary: () => void;
 };
 
-export const useCatStore = create<TCatStoreState>()((set, get) => ({
-  cats: {
-    bigCats: 0,
-    smallCats: 0,
-  },
-  increaseBigCats: () =>
-    set((state) => ({
-      cats: { ...state.cats, bigCats: state.cats.bigCats + 1 },
-    })),
-  increaseSmallCats: () =>
-    set((state) => ({
-      cats: { ...state.cats, smallCats: state.cats.smallCats + 1 },
-    })),
-  summary: () => {
-    const total = get().cats.bigCats + get().cats.smallCats;
-    return `There are ${total} cats in total. `;
-  },
-}));
+export const useCatStore = createSelectors(
+  create<TCatStoreState>()((set, get) => ({
+    cats: {
+      bigCats: 0,
+      smallCats: 0,
+    },
+    increaseBigCats: () =>
+      set((state) => ({
+        cats: { ...state.cats, bigCats: state.cats.bigCats + 1 },
+      })),
+    increaseSmallCats: () =>
+      set((state) => ({
+        cats: { ...state.cats, smallCats: state.cats.smallCats + 1 },
+      })),
+    summary: () => {
+      const total = get().cats.bigCats + get().cats.smallCats;
+      return `There are ${total} cats in total. `;
+    },
+  }))
+);
